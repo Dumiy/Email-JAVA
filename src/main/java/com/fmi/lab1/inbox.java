30128ff7211d10aa61223b3c7e520584f5868d7e
Dumiy <45998406+Dumiy@users.noreply.github.com>
2019-06-02 20:31:12 +0300

    Add files via upload

---
+++ src/main/java/com/fmi/lab1/inbox.java
@@ -0,0 +1,10 @@
+package com.fmi.lab1;
+import java.util.ArrayList;
+import com.fmi.lab1.email;
+import com.fmi.lab1.account;
+
+
+public class inbox {
+    private ArrayList<email> Inbox = new ArrayList<email>();
+
+}
