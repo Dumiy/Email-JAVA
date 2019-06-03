cd588b8b0fa73548b289497fdec5e733199f1ef1
Dumiy <dumitrascu.claudiu.cristian@gmail.com>
2019-06-03 22:36:45 +0300

    Added swing

---
+++ src/main/java/GUI/MainMenu.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MainMenu {
+    private JPanel email;
+    private JPanel inbox;
+    private JPanel account;
+    private JPanel dbOps;
+    private JPanel EMAIL;
+}
